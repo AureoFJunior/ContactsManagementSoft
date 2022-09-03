79beb2f5d289674d86738596adcaa3c81fc8ef14
Aureo da Silva Fajardo Junior <junior7f@hotmail.com>
2022-09-03 10:46:12 -0300

    CRUD updates

--- ConManagement/Controllers/ContactController.cs
+++ ConManagement/Controllers/ContactController.cs
@@ -1,4 +1,5 @@
-﻿using ContactsManagementSoft.Models;
+﻿using ContactsManagementSoft.DataBase;
+using ContactsManagementSoft.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +8,21 @@ namespace ContactsManagementSoft.Controllers
     public class ContactController : Controller
     {
         private readonly ILogger<ContactController> _logger;
+        private readonly Context Context;
 
-        public ContactController(ILogger<ContactController> logger)
+        public ContactController(ILogger<ContactController> logger, [FromServices] Context context)
         {
             _logger = logger;
+            Context = context;
         }
 
         public IActionResult Index()
         {
-            List<ContactModel> contacts = new List<ContactModel>() { new ContactModel() { ID = 1, Contact = "PH GOSTOSO", Name = "PH", Email = "safadao@gmail.com" } };
+            List<ContactModel> contacts = Context.ContactModel.ToList();
+
+            if (contacts == null)
+                contacts = new List<ContactModel>();
+
             return View(contacts);
         }
 
@@ -24,14 +31,54 @@ namespace ContactsManagementSoft.Controllers
             return View();
         }
 
+        public IActionResult Create(ContactModel model)
+        {
+            try
+            {
+                var contactCreated = Context.ContactModel.Add(model);
+                Context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex) { return View(); }
+        }
+
         public IActionResult Edit()
         {
             return View();
         }
 
-        public IActionResult Details()
+        public IActionResult Edit(ContactModel model)
+        {
+            try
+            {
+                var contactUpdated = Context.ContactModel.Update(model);
+                Context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex) { return View(); }
+        }
+
+        public IActionResult Delete()
         {
             return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                ContactModel model = Context.ContactModel.Find(id);
+                var contactCreated = Context.ContactModel.Remove(model);
+                Context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex) { return View(); }
+        }
+
+        public IActionResult Details(int id)
+        {
+            var model = Context.ContactModel.Find(id);
+            return View(model);
         }
 
     }
